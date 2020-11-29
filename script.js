2fba1f765fc25da9a308e2dd40001f8703906405
Simon Fishenzone <69964802+fishenzone@users.noreply.github.com>
2020-11-29 23:31:27 +0300

    init com

--- script.js
+++ script.js
@@ -1,95 +1,117 @@
-var margin = { top: 10, right: 5, bottom: 10, left: 100 },
-  width = 1400 - margin.left - margin.right,
-  height = 900 - margin.top - margin.bottom;
+var json =
+  'https://cdn.rawgit.com/freeCodeCamp/testable-projects-fcc/a80ce8f9/src/data/tree_map/movie-data.json';
+var tooltip = d3.select('body').append('div').attr('id', 'tooltip');
+var format = d3.format('$,.2s');
+var colors = [
+  ['#78c5d6', 'Action'],
+  ['#459ba8', 'Drama'],
+  ['#79c267', 'Adventure'],
+  ['#c5d647', 'Family'],
+  ['#f5d63d', 'Animation'],
+  ['#f28c33', 'Comedy'],
+  ['#e868a2', 'Biography'],
+];
 
-var svg = d3
-  .select('body')
-  .append('svg')
-  .attr('width', width)
-  .attr('height', height);
+var width = 1100;
+var height = 650;
+var margin = { top: 200, right: 0, bottom: 0, left: 0 };
 
-var tooltip = d3.select('body').append('div').attr('class', 'tooltip').style({
-  background: 'orangered',
-  color: 'white',
-  width: '90px',
-});
-
-d3.json(
-  'https://raw.githubusercontent.com/DealPete/forceDirected/master/countries.json',
-  function (data) {
-    var nodes = data['nodes'];
-    var links = data['links'];
-
-    var force = d3.layout
-      .force()
-      .size([width, height])
-      .nodes(d3.values(nodes))
-      .links(links)
-      .on('tick', tick)
-
-      .linkDistance(300)
-
-      .start();
-
-    var link = svg
-      .selectAll('.link')
-      .data(links)
-      .enter()
-      .append('line')
-      .attr('class', 'link');
-
-    var node = d3
-      .select('#flags')
-      .selectAll('img')
-      .data(force.nodes())
-      .enter()
-      .append('img')
-      .attr('class', function (d) {
-        return 'flag flag-' + d.code;
-      })
-      .on('mouseover', mouseoverHandler)
-      .on('mousemove', mouseMoving)
-      .on('mouseout', mouseoutHandler);
-
-    function tick(e) {
-      node
-        .style('left', function (d) {
-          return d.x + 'px';
-        })
-        .style('top', function (d) {
-          return d.y + 'px';
-        })
-        .call(force.drag);
-
-      link
-        .attr('x1', function (d) {
-          return d.source.x;
-        })
-        .attr('y1', function (d) {
-          return d.source.y;
-        })
-        .attr('x2', function (d) {
-          return d.target.x;
-        })
-        .attr('y2', function (d) {
-          return d.target.y;
-        });
-    }
-
-    function mouseoverHandler(d) {
-      tooltip.transition().style('opacity', 0.9);
-      tooltip.html('<p>' + d['country'] + '</p>');
-    }
-
-    function mouseoutHandler(d) {
-      tooltip.transition().style('opacity', 0);
-    }
-
-    function mouseMoving(d) {
-      tooltip
-        .style('top', d3.event.pageY - 10 + 'px')
-        .style('left', d3.event.pageX + 10 + 'px')
-        .style('color', 'white');
+var fill = (d) => {
+  for (let i in colors) {
+    if (d === colors[i][1]) {
+      return colors[i][0];
     }
   }
-);
+};
+
+var svg = d3.select('#d3').attr('height', height).attr('width', width);
+
+var treemap = d3
+  .treemap()
+  .size([width, height])
+  .paddingInner(0.2)
+  .paddingOuter(0.8);
+
+d3.json(json, (error, dataset) => {
+  if (error) throw error;
+
+  // Create Hierarchy
+  var node = d3.hierarchy(dataset);
+  node.sum((d) => d.value).sort((a, b) => a.value > b.value);
+  treemap(node);
+
+  var group = svg
+    .selectAll('g')
+    .data(node.leaves())
+    .enter()
+    .append('g')
+    .attr('class', 'group')
+    .attr('transform', (d) => 'translate(' + d.x0 + ',' + d.y0 + ')');
+  // .attr('style', d => console.log(d));
+
+  var tile = group
+    .append('rect')
+    .attr('class', 'tile')
+    .attr('width', (d) => d.x1 - d.x0)
+    .attr('height', (d) => d.y1 - d.y0)
+    .attr('data-name', (d) => d.data.name)
+    .attr('data-category', (d) => d.data.category)
+    .attr('data-value', (d) => d.data.value)
+    .attr('fill', (d) => fill(d.data.category))
+    .on('mousemove', (d, i) => {
+      tooltip
+        .attr('data-value', d.data.value)
+        .style('left', d3.event.pageX + 20 + 'px')
+        .style('top', d3.event.pageY + 20 + 'px')
+        .style('display', 'inline-block')
+        .html(() => {
+          return `
+                                        Category:  ${d.data.category}<br/>
+                                        Name: ${d.data.name}<br/>
+                                        Value: ${format(d.data.value)}`;
+        });
+    })
+    .on('mouseout', (d) => tooltip.style('display', 'none'));
+
+  var text = group
+    .append('text')
+    .attr('class', 'tile-text')
+    .attr('dx', 5)
+    .attr('dy', '1.5em')
+    .attr('width', (d) => d.x1 - d.x0)
+    .text((d) => d.data.name);
+
+  var text = group
+    .append('text')
+    .attr('class', 'tile-value')
+    .attr('dx', 5)
+    .attr('dy', '2.8em')
+    .attr('width', (d) => d.x1 - d.x0)
+    .text((d) => format(d.data.value));
+});
+
+/**
+ * Create Legend
+ */
+var legend = d3
+  .select('#legend')
+  .attr('height', margin.top)
+  .attr('width', width)
+  .attr('id', 'legend');
+
+colors.forEach((color, i) => {
+  legend
+    .append('rect')
+    .attr('x', i * (width / colors.length))
+    .attr('y', 25)
+    .attr('height', 20)
+    .attr('width', 20)
+    .attr('fill', color[0])
+    .attr('class', 'legend-item');
+
+  legend
+    .append('text')
+    .attr('x', i * (width / colors.length) + 30)
+    .attr('y', 40)
+    .text(color[1]);
+});
